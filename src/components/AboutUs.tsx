e51c3b746557a4652748afb47c0d136c6661bfe4
Gyorgy <gyorgy.herbszt@sonardesign.hu>
2024-10-21 19:24:10 +0200

    about us image commented

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,9 +6,10 @@ const AboutUs: React.FC = () => {
     <section className="about-us">
       <div className="container">        
         <div className="about-us__content">
-          <div className="about-us__image">
-            <img src="/assets/images/" alt="About Us" />
-          </div>
+          <div className="about-us__image"
+            // <img src="/assets/images/" alt="About Us" />
+            >
+            </div>
           <div className="about-us__text">
             <h1>Lorem impsum főcím</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
