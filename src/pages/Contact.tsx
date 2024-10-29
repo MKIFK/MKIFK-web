6ff1d9f3cc665cef17c9bfd9bd49fd6b35416a4b
Gyorgy <gyorgy.herbszt@sonardesign.hu>
2024-10-29 20:43:14 +0100

    contact fixes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,21 +4,23 @@ const Contact: React.FC = () => {
   return (
     <div className="contact">
       <div className="container">
-        <h1>Kapcsolat</h1>
+        <h1 className='section-title'>Kapcsolat</h1>
         <div className="contact__content">
           <div className="contact__info">
-          <br></br>
-          <h3>
-            MKIFK Magyar Közlönykiadó és Igazságügyi Fordítóközpont <br></br>
-             Zártkörűen Működő Részvénytársaság
-          </h3>
-          <br></br>
-          <p>székhelye: 1085 Budapest, Somogyi Béla u. 6.;</p>
-          <p>cégjegyzékszám: 01-10-042469;</p>
-          <p>adószám: 10941908-2-42;</p>
-          <p>képviseli: Németh Balázs Sándor vezérigazgató</p>
-          <p>titkárság: titkarsag@mhk.hu</p>
-            
+            <p>
+              MKIFK Magyar Közlönykiadó és Igazságügyi Fordítóközpont <br></br>
+              Zártkörűen Működő Részvénytársaság
+            </p>
+            <br></br>
+            <p>székhelye: 1085 Budapest, Somogyi Béla u. 6.;</p>
+            <p>cégjegyzékszám: 01-10-042469;</p>
+            <p>adószám: 10941908-2-42;</p>
+            <p>képviseli: Németh Balázs Sándor vezérigazgató</p>
+            <p>titkárság:
+              <a href="mailto:your-email@example.com?subject=Hello&body=I wanted to reach out to you.">
+              titkarsag@mhk.hu
+              </a>
+            </p>
           </div>
         </div>
       </div>
