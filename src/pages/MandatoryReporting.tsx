578c17406d6df327eea7e7942e1ec9ff6d1ef9e5
Gyorgy <gyorgy.herbszt@sonardesign.hu>
2024-11-05 17:55:05 +0100

    all merged

--- src/pages/MandatoryReporting.tsx
+++ src/pages/MandatoryReporting.tsx
@@ -9,12 +9,10 @@ const MandatoryReporting: React.FC = () => {
         <h1>Kötelező adatszolgáltatás</h1>
         <div className="mandatory-reporting__content">
           <p>A 2024-re vonatkozó közérdekű adatok az alábbi linkeken érhetőek el:</p>
-
           <ul>
             <li><a href="https://mhk.hu/">Magyar Közlöny / Közadat</a></li>
             <li><a href="https://www.offi.hu/cegunkrol/kozerdeku-adatok">OFFI / Közadat</a></li>
           </ul>
-          
           <p>További információkért kérjük, vegye fel a kapcsolatot titkárságunkkal.</p>
         </div>
       </div>
